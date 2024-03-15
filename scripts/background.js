f4f2d1db0306730826e1f236f9a0dfd923a5be44
Majed Alghoul <majed.alghoul3@gmail.com>
2024-03-15 23:58:20 +0200

    cleanup & icon fix & added url grapping

--- scripts/background.js
+++ scripts/background.js
@@ -1,54 +1,60 @@
-// Rule to block all URLs
 let blockRule = {
-    "id": 5,
-    "priority": 1,
-    "action": {
-        "type": "redirect",
-        "redirect": {"extensionPath":  "/pages/loading.html"}
-    },
-    "condition": {
-      "urlFilter": "*",
-      "resourceTypes": ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-    }
+  "id": 5,
+  "priority": 1,
+  "action": {
+    "type": "redirect",
+    "redirect": { "extensionPath": "/pages/loading.html" }
+
+  },
+  "condition": {
+    "urlFilter": "*",
+    "resourceTypes": ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+  }
 };
 
-// Save whitelist to storage
-//chrome.storage.sync.set({whitelist: ['apple.com', 'google.com', 'microsoft.com']}, function() {
-//    console.log('Whitelist saved');
-//});
+let currentURL = '';
 
-// Load whitelist from storage
-chrome.storage.sync.get(['whitelist'], function(result) {
-    if (result.whitelist) {
-      let whitelist = result.whitelist;
-      console.log('Whitelist loaded: ' + whitelist);
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    currentURL = details.url;
+    console.log('Current URL:', currentURL);
+  },
+  { urls: ['<all_urls>'] }
+);
 
-      let allowRules = whitelist.map((website, index) => {
-        return {
-          "id": index + 1,
-          "priority": 2,
-          "action": {
-            "type": "allow"
-          },
-          "condition": {
-            "urlFilter": `*${website}/*`,
-            "resourceTypes": ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-          }
-        };
-      });
-  
-      chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [...allowRules.map(rule => rule.id), 5],
-        addRules: [...allowRules, blockRule]
-      }).catch((error) => {
-        console.error('Error updating rules:', error);
-      });
-    } else {
-      console.log('Whitelist not found');
-    }
-  });
+chrome.storage.sync.set({ whitelist: ['apple.com', 'microsoft.com'] }, function () {
+  console.log('Whitelist saved');
+});
 
+chrome.storage.sync.get(['whitelist'], function (result) {
+  if (result.whitelist) {
+    let whitelist = result.whitelist;
+    console.log('Whitelist loaded: ' + whitelist);
 
+    let allowRules = whitelist.map((website, index) => {
+      return {
+        "id": index + 1,
+        "priority": 2,
+        "action": {
+          "type": "allow"
+        },
+        "condition": {
+          "urlFilter": `*${website}/*`,
+          "resourceTypes": ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+        }
+      };
+    });
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [...allowRules.map(rule => rule.id), 5],
+      addRules: [...allowRules, blockRule]
+    }).catch((error) => {
+      console.error('Error updating rules:', error);
+    });
+  } else {
+    console.log('Whitelist not found');
+  }
+});
 
 //chrome.storage.sync.remove('whitelist', function() {
 //    console.log('Whitelist removed');
